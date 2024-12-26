9d7470e8a960d984895b714e3b4d37f4a48c7e6e
Jordan Reyes <dawizkid2006@gmail.com>
2024-12-26 18:30:52 -0500

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -24,15 +24,20 @@ const editModalNameInput = editModal.querySelector("#profile-name-input");
 const editModalDescriptionInput = editModal.querySelector("#profile-description-input");
 
 const cardTemplate = document.querySelector("#card-template");
-const cardList = document.querySelector(".cards__list")
+const cardsList = document.querySelector(".cards__list")
 
 function getCardElement(data){
-    console.log(data)
-    const cardElement = cardTemplate.content.querySelector(".card").cloneNode(true);
+    const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
 
-    const cardNameEl = cardElement.querySelector(".card__title")
+    const cardNameEl = cardElement.querySelector(".card__title");
+    const cardImageEl = cardElement.querySelector(".card__image");
 
-    cardNameEl.value = data.name;
+    cardNameEl.textContent = data.name;
+    cardImageEl.src = data.link;
+    cardImageEl.alt = data.name;
+    
 
     return cardElement;
 }
@@ -59,6 +64,6 @@ editModalCloseBtn.addEventListener("click", closeModal);
 editFormElement.addEventListener("submit", handleEditFormSubmit);
 
 for (i = 0; i < initialCards.length; i++) {
-    const cardElement = getCardElement(initialCards[i])
-    cardList.prepend(cardElement);
-}
+    const cardElement = getCardElement(initialCards[i]);
+    cardsList.prepend(cardElement);
+  }
