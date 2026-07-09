0c3ec5441bf416350a7f2748c6b4e2860f242547
CR0S5EDX3N0 <dawizkid2006@gmail.com>
2026-07-09 17:09:16 -0400

    some commit message

--- scripts/validation.js
+++ scripts/validation.js
@@ -75,4 +75,4 @@ function resetValidation(formElement, settings) {
   formElement.reset();
   toggleButtonState(inputList, buttonElement, settings);
 }
-
+export { enableValidation, settings, resetValidation };
